8daf4c4efec087d362b5b6a74e307060dc4dc1c4
Georgi <g.ivanov@reply.com>
2024-04-02 12:20:44 +0100

    test commit

--- AoC/Day 1 Sonar Sweep/Program.cs
+++ AoC/Day 1 Sonar Sweep/Program.cs
@@ -4,13 +4,13 @@ namespace Day_1_Sonar_Sweep
 {
     class Program
     {
-       public static int numberOfIncreases(string data) { 
+       public static int numberOfIncreases(string data) {
             var input = data.Split();
             {
                 var count = 0;
                 for (int i = 1; i < input.Length; i++)
                 {
-
+                    // Test
                     if (int.Parse(input[i]) > int.Parse(input[i - 1]))
                     {
                         count++;
